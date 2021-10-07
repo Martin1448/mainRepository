eacf71b7dc2dfc7ade9dca16434f835814df4a91
Martin1448 <91890196+Martin1448@users.noreply.github.com>
2021-10-07 16:12:23 +0300

    Добавлены классы и функции

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,11 +7,11 @@ public class MyFirstProgram {
         hello("User");
         hello("Vladislav");
 
-        double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь треугольника со сторонами " + a +" и " + b + " = " +area(a,b));
+        Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь треугольника со сторонами " + r.a +" и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
@@ -20,12 +20,7 @@ public class MyFirstProgram {
 
     }
 
-	public static double area(double len) {
-    	return len * len;
 
-	}
-	public static double area(double a, double b ) {
-    	return a + b;
-	}
+
 }
 
